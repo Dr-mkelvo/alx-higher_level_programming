50261ed2426eefa8f65badac60fdaf14c4fb1b2e
Mwiyeria Kelvin <118157498+Dr-mkelvo@users.noreply.github.com>
2023-03-12 23:58:05 +0300

    Update 1-element_at.py
    remove the return value

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx in <0 or => len(my_list) -1:
-        return None
+    if idx in < 0 or => len(my_list) -1:
+        return 
     return my_list[idx]
-
