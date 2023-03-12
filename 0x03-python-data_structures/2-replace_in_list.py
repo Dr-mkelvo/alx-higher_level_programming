f9ad1c620a0310a543b48ab122e747a9ece84f62
Dr-mkelvo <kelvincent002@gmail.com>
2023-03-12 14:18:27 -0700

    Fixed a few logic bugs

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= my_list:
-        return my_list[idx]
-    return my_list[element]
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
