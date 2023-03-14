3cb5799a0dc2402ba004fbafb4e01a1ca1be6897
Dr-mkelvo <kelvincent002@gmail.com>
2023-03-14 09:45:04 -0700

    Fixed pycodestyle and syntax errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,10 +6,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_a = (tuple_a[0], 0)
 
-    
     if len(tuple_b) < 2:
         if len(tuple_b) == 0:
             tuple_b = (0, 0)
         else:
             tuple_b = (tuple_b[0], 0)
-    return(tuple_a[0] + tuple_b[0], tuple_a[1], tuple_b[1])
+    results = (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
+    return results
