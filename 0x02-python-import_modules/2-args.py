8095b426c64d55467c1b238b1707809dbc0f4f7e
Dr-mkelvo <kelvincent002@gmail.com>
2023-03-19 10:53:18 +0300

    fixed pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    
+
     import sys
 
     count = len(sys.argv) - 1
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
