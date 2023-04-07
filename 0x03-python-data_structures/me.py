9d392c95abeef5f066ebbca9d335495157145195
Dr-mkelvo <kelvincent002@gmail.com>
2023-04-07 19:20:35 +0300

    updates made

---
+++ 0x03-python-data_structures/me.py
@@ -0,0 +1,5 @@
+#!/bin/usr/python3
+def elemement_at(my_list, idx):
+    if idx < 0 and >= len(my_list):
+        return None
+    return my_list[idx]
---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/usr/python3
+"""Defining a class (Rectange)"""
+
+class Rectangle:
+    """Representing the Rectangle"""
+
+    pass
