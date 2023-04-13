d9254b9542c2ff77929c75ccb4374f2ed1b300de
Dr-mkelvo <kelvincent002@gmail.com>
2023-04-13 23:20:24 +0300

    python  notes

---
+++ learn_2_program/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for num in range(98):
+    print("{} = {}".format(num, hex(num)))
---
+++ learn_2_program/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/bin/usr/python3
+def uppercase(str):
+    if ord(str) > 100 and ord(str) < 133:
+        return True
+    else:
+        return False
