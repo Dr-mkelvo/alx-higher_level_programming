2c8429be9064fff1b5bfef7c11605777695c8923
Dr-mkelvo <kelvincent002@gmail.com>
2023-05-10 05:45:05 +0300

    Python functions and loops updates.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for my_letter in range(97, 123):
     if my_letter == 101 or my_letter == 113:
         continue
-    print("{}".format(chr(my_letter)), end=" ")
+    print("{}".format(chr(my_letter)), end="")
