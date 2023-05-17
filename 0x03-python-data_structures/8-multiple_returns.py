bfd7b58f5819faf52db14c8fdc69185a0dd77d20
Dr-mkelvo <kelvincent002@gmail.com>
2023-05-17 05:48:11 +0300

    Fixed code2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,5 @@
 def multiple_returns(sentence):
     for element in sentence:
         if len(sentence) == 0:
-            return (0, none)
-        else:
-            return (len(sentence), sentence[0])
+            return (0, None)
+        return (len(sentence), sentence[0])
