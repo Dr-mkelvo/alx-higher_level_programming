735e89f1dc6deba14652bafdf6711ff1cea5e357
Dr-mkelvo <kelvincent002@gmail.com>
2023-05-17 05:40:14 +0300

    Fixed pycodestyle bug2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     for element in sentence:
         if len(sentence) == 0:
-            return (0, None)
+            return (0, none)
         else:
             return (len(sentence), sentence[0])
